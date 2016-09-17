25af64b2f91ba966c6af35f8b2440ee8744ab847
Rachel Jane Morris <racheljmorris@gmail.com>
2016-09-17 14:42:58 -0500

    WIP Linked List

--- Resources/Example Code/Linked Lists/Single Linked List/SingleLinkedList.hpp
+++ Resources/Example Code/Linked Lists/Single Linked List/SingleLinkedList.hpp
@@ -20,24 +20,105 @@ class SingleLinkedList
     public:
     SingleLinkedList()
     {
+        cout << "\t(Constructor)" << endl;
         m_count = 0;
+        m_ptrFirst = nullptr;
+        m_ptrLast = nullptr;
     }
 
     ~SingleLinkedList()
     {
+        cout << "\t(Destructor)" << endl;
         ClearList();
     }
 
     void ClearList()
     {
+        while ( m_ptrFirst != nullptr )
+        {
+            cout << "\t(ClearList) List size: " << m_count << endl;
+            FreeNode( m_ptrFirst );
+        }
     }
 
     void AddItem( T data )
     {
+        Node<T>* newNode = new Node<T>;
+        newNode->data = data;
+
+        if ( m_count == 0 )
+        {
+            // This is the only node so point both
+            // the beginning and ending at the new node.
+            m_ptrFirst = m_ptrLast = newNode;
+            m_ptrFirst->ptrNext = m_ptrLast;
+        }
+        else
+        {
+            m_ptrLast->ptrNext = newNode;
+            m_ptrLast = newNode;
+        }
+
+        m_count++;
     }
 
-    void RemoveItem( T data )
+    void RemoveItem( const T& data )
     {
+        // Find the item in question
+        Node<T>* dataNode = nullptr;
+        Node<T>* prevNode = nullptr;
+
+        // Successfully found item
+        if ( FindItem( data, dataNode, prevNode ) )
+        {
+            // Is this the first item?
+            if ( dataNode == m_ptrFirst )
+            {
+                cout << "\t(RemoveItem) It's the first item" << endl;
+
+                // Update the new first pointer.
+                m_ptrFirst = dataNode->ptrNext;
+            }
+            // Is this the last item?
+            else if ( dataNode == m_ptrLast )
+            {
+                cout << "\t(RemoveItem) It's the last item" << endl;
+
+                // Update the last pointer to the second to last item.
+                m_ptrLast = prevNode;
+            }
+            // Otherwise...
+            else
+            {
+                cout << "\t(RemoveItem) Center item" << endl;
+
+                // Have the previous node skip over the node to remove.
+                prevNode->ptrNext = dataNode->ptrNext;
+            }
+
+            cout << "\t(RemoveItem) Before Free: " << endl;
+            Print();
+
+            // Free the node
+            cout << "\t(RemoveItem) Free node " << dataNode << endl;
+            delete dataNode;
+            dataNode = nullptr;
+            m_count--;
+
+            cout << "\t(RemoveItem) After Free: " << endl;
+            Print();
+        }
+    }
+
+    void FreeNode( Node<T>*& node )
+    {
+        cout << "\t(FreeNode) Free node: " << node << " = " << node->data << endl;
+        if ( node != nullptr )
+        {
+            delete node;
+            node = nullptr;
+            m_count--;
+        }
     }
 
     int Size()
@@ -45,14 +126,59 @@ class SingleLinkedList
         return m_count;
     }
 
+    bool FindItem( const T& data, Node<T>*& dataNode, Node<T>*& prevNode )
+    {
+        cout << "\t(FindItem) Find item \"" << data << "\"" << endl;
+
+        dataNode = m_ptrFirst;
+        prevNode = nullptr;
+        int counter = 0;
+
+        while ( dataNode != nullptr )
+        {
+            if ( dataNode->data == data )
+            {
+                break;
+            }
+
+            prevNode = dataNode;
+            dataNode = dataNode->ptrNext;
+            counter++;
+        }
+
+        if ( dataNode == nullptr )
+        {
+            cout << "\t(FindItem) Didn't find item" << endl;
+            // Didn't find item in list
+            return false;
+        }
+        else
+        {
+            cout << "\t(FindItem) Found at position " << counter << endl;
+            cout << "\t(FindItem) Data node: " << dataNode << " (" << dataNode->data << ")" << endl;
+            if ( dataNode != m_ptrFirst )
+            {
+                cout << "\t(FindItem) Prev node: " << prevNode << " (" << prevNode->data << ")" << endl;
+            }
+
+            // Successfully found item,
+            // dataNode and prevNode should be
+            // pointing at the correct nodes.
+            return true;
+        }
+    }
+
     void Print()
     {
+		cout << "List Size: " << m_count << endl;
         Node<T>* ptrCurrent = m_ptrFirst;
 
+        int counter = 0;
         while ( ptrCurrent != nullptr )
         {
-            cout << ptrCurrent->data << endl;
+            cout << counter << " = " << ptrCurrent->data << " (" << ptrCurrent << ")" << endl;
             ptrCurrent = ptrCurrent->ptrNext;
+            counter++;
         }
     }
 
--- Resources/Example Code/Linked Lists/Single Linked List/main.cpp
+++ Resources/Example Code/Linked Lists/Single Linked List/main.cpp
@@ -5,5 +5,38 @@ using namespace std;
 
 int main()
 {
+    SingleLinkedList<string> myList;
+
+    cout << "--------------------------------" << endl;
+    cout << "Insert letters A - G" << endl;
+    myList.AddItem( "A" );
+    myList.AddItem( "B" );
+    myList.AddItem( "C" );
+    myList.AddItem( "D" );
+    myList.AddItem( "E" );
+    myList.AddItem( "F" );
+    myList.AddItem( "G" );
+//    myList.Print();
+
+    cout << "--------------------------------" << endl;
+    cout << "Remove last item" << endl;
+    myList.RemoveItem( "G" );
+//    myList.Print();
+
+    cout << "--------------------------------" << endl;
+    cout << "Remove first item" << endl;
+    myList.RemoveItem( "A" );
+////    myList.Print();
+//
+//    cout << "--------------------------------" << endl;
+//    cout << "Remove center item" << endl;
+//    myList.RemoveItem( "D" );
+////    myList.Print();
+////
+//    cout << "--------------------------------" << endl;
+//    cout << "Remove item that isn't in the list" << endl;
+//    myList.RemoveItem( "Z" );
+//    myList.Print();
+
     return 0;
 }

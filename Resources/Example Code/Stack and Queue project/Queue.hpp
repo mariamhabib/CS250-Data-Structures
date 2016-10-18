81515ab0395a1ae2d9012105cb289ccb47dff2be
Rachel Jane Morris <racheljmorris@gmail.com>
2016-10-18 16:24:06 -0500

    DoublyLinkedList Stack and Queue projects

---
+++ Resources/Example Code/Stack and Queue project/Queue.hpp
@@ -0,0 +1,39 @@
+#ifndef QUEUE_HPP
+#define QUEUE_HPP
+
+#include "DoublyLinkedList.hpp"
+
+template <typename T>
+class Queue
+{
+    public:
+    void Enqueue( T data )
+    {
+        m_list.PushBack( data );
+    }
+
+    void Dequeue()
+    {
+        m_list.PopFront();
+    }
+
+    T& Front()
+    {
+        return m_list.GetFront();
+    }
+
+    int GetSize()
+    {
+        return m_list.GetSize();
+    }
+
+    void Display()
+    {
+        m_list.Display();
+    }
+
+    private:
+    DoublyLinkedList<T> m_list;
+};
+
+#endif

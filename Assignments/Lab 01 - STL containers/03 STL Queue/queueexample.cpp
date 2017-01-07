cb0cb9d790369104753b5d90c4a23e9f042295cb
Rachel Jane Morris <racheljmorris@gmail.com>
2017-01-06 19:40:40 -0600

    Starter code added for first lab

---
+++ Assignments/Lab 01 - STL containers/03 STL Queue/queueexample.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    float balance = 0.0;
+    // Create a queue of floats, named transactions
+    
+    // Push some + and - floats into the transactions queue.
+    
+    // Loop through all the transactions - while the queue is empty.
+    // Within the loop, whatever amount is at the front of the queue,
+    // add it to the balance and then pop it off the queue.
+
+    cout << "Final balance: $" << balance << endl;
+    
+    return 0;
+}
---
+++ Assignments/Lab 01 - STL containers/05 STL Map/mapexample.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main()
+{
+    // Create a map that uses two strings. Name it colors.
+
+    
+    // Populate several colors and their hex codes
+    // red = FF0000, green = 00FF00, blue = 0000FF
+    // magenta = FF00FF, yellow = FFFF00, cyan = 00FFFF
+
+    
+    string color;
+    cout << "Enter a color: ";
+    cin >> color;
+    
+    // Display the hex value of the color, using the user's input as a key.
+
+    
+    return 0;
+}

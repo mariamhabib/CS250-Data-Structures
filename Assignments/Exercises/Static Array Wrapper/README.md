eb7468707ef79071aea089a7f982e6143ed7641e
Rachel "Wil Sha" Morris <racheljmorris@gmail.com>
2017-02-01 10:44:40 -0600

    WIP project

---
+++ Assignments/Exercises/Static Array Wrapper/README.md
@@ -0,0 +1,35 @@
+# Homework Exercise: Wrapping a Static Array
+
+## Downloads
+
+Download the starter files for this project.
+
+* Visual Studio project
+* Code::Blocks project
+
+Or you can download the files directly on GitHub:
+
+* []()
+
+## Rules
+
+* Team size: 1 person
+    * You can brainstorm with others - that is, you can verbally discuss the problem.
+    * You cannot copy another person's work. Code will be checked against other submissions with a diff tool. If a copy is found, both people will receive a 0 on the assignment.
+* Open book / open note - do research as-needed.
+
+## Overview
+
+The starter files contain the stub for the Smart Array, with functions
+that need to be implemented.
+
+The project also contains tester files that you can use to judge
+whether your functions are working properly.
+
+## Turn in
+
+Once you're done, upload all source files (.cpp, .hpp, .h) to the class dropbox
+and up on your GitHub page.
+
+---
+
---
+++ Assignments/Exercises/Static Array Wrapper/Starter files/SmartStaticArray.cpp
@@ -0,0 +1,76 @@
+#include "SmartStaticArray.hpp"
+
+SmartStaticArray::SmartStaticArray()
+{
+	m_itemCount = 0;
+}
+
+bool SmartStaticArray::Push( const string& newItem )
+{
+	return false; // temp
+}
+
+
+bool SmartStaticArray::Insert( int index, const string& newItem )
+{
+	return false; // temp
+}
+
+
+bool SmartStaticArray::Extend( const SmartStaticArray& other )
+{
+	return false; // temp
+}
+
+
+bool SmartStaticArray::Pop()
+{
+	return false; // temp
+}
+
+
+bool SmartStaticArray::Remove( int index )
+{
+	return false; // temp
+}
+
+
+string SmartStaticArray::Get( int index ) const
+{
+	return "temp"; // temp
+}
+
+
+int SmartStaticArray::Size() const
+{
+	return -1; // temp
+}
+
+
+bool SmartStaticArray::IsFull() const
+{
+	return false; // temp
+}
+
+bool SmartStaticArray::IsEmpty() const
+{
+	return false; // temp
+}
+
+
+string SmartStaticArray::operator[]( int index )
+{
+	return "temp"; // temp
+}
+
+
+SmartStaticArray SmartStaticArray::operator=( const SmartStaticArray& other )
+{
+    SmartStaticArray blorp; // temp
+    return blorp; // temp
+}
+
+bool SmartStaticArray::operator==( const SmartStaticArray& other )
+{
+	return false; // temp
+}

cd4c3f721b1e962596653b22b153e5c0b01c37dc
Rachel "Wil Sha" Morris <racheljmorris@gmail.com>
2018-02-13 15:52:37 -0600

    Updates to List and Test to help out

--- Assignments/Projects/starter files/PROJECT1-LINEAR-STRUCTURES/List.hpp
+++ Assignments/Projects/starter files/PROJECT1-LINEAR-STRUCTURES/List.hpp
@@ -1,6 +1,9 @@
 #ifndef _LIST_HPP
 #define _LIST_HPP
 
+#include <iostream>
+using namespace std;
+
 const int ARRAY_SIZE = 100;
 
 template <typename T>
@@ -18,11 +21,23 @@ private:
 
     bool ShiftLeft( int atIndex )
     {
+        if ( atIndex < 0 || atIndex > m_itemCount )
+        {
+            return false;
+        }
+
+        for ( int i = atIndex + 1; i < m_itemCount; i++ )
+        {
+            m_arr[i-1] = m_arr[i];
+        }
+
+        return true;
     }
 
 public:
     List()
     {
+        m_itemCount = 0;
     }
 
     ~List()
@@ -32,68 +47,142 @@ public:
     // Core functionality
     int     Size() const
     {
+        return -1; // placeholder
     }
 
     bool    IsEmpty() const
     {
+        return false; // placeholder
     }
 
     bool    IsFull() const
     {
+        return false; // placeholder
     }
 
     bool    PushFront( const T& newItem )
     {
+        return false; // placeholder
     }
 
     bool    PushBack( const T& newItem )
     {
+        if ( IsFull() { return false; }
+
+        m_arr[m_itemCount] = item;
+        m_itemCount++;
+        return true;
     }
 
     bool    Insert( int atIndex, const T& item )
     {
+        return false; // placeholder
     }
 
     bool    PopFront()
     {
+        return false; // placeholder
     }
 
     bool    PopBack()
     {
+        if ( IsEmpty() ) { return false; }
+
+        // lazy deletion
+        m_itemCount--;
+        return true;
     }
 
-    bool    Remove( const T& item )
+    bool    RemoveItem( const T& item )
     {
+        if ( IsEmpty() )
+        {
+            return false;
+        }
+
+        // One way you can do this
+        int removeAtIndex[ARRAY_SIZE];
+        int j = 0;
+
+        // Keep track of indices to remove
+        for ( int i = 0; i < Size(); i++ )
+        {
+            if ( m_arr[i] == item )
+            {
+                removeAtIndex[j] = i;
+                j++;
+            }
+        }
+
+        // Remove all indices we recorded
+        for ( int i = 0; i < j; i++ )
+        {
+            RemoveIndex( removeAtIndex[i] );
+        }
     }
 
-    bool    Remove( int atIndex )
+    bool    RemoveIndex( int atIndex )
     {
+        return false; // placeholder
     }
 
     void    Clear()
     {
+        // lazy deletion
+        m_itemCount = 0;
     }
 
     // Accessors
     T*      Get( int atIndex )
     {
+        if ( atIndex < 0 || atIndex >= m_itemCount )
+        {
+            return nullptr;
+        }
+
+        return &m_arr[ atIndex ];
     }
 
     T*      GetFront()
     {
+        return nullptr; // placeholder
     }
 
     T*      GetBack()
     {
+        return nullptr; // placeholder
     }
 
     // Additional functionality
     int     GetCountOf( const T& item ) const
     {
+        return 0; // placeholder
     }
 
     bool    Contains( const T& item ) const
     {
+        return ( GetCountOf(item) > 0 );
+    }
+
+    // Helper function
+    void Display()
+    {
+        cout << "\t List size: " << Size() << endl;
+        for ( int i = 0; i < Size(); i++ )
+        {
+            T* item = Get(i);
+
+            cout << "\t " << i << " = ";
+
+            if ( item == nullptr )
+            {
+                cout << "nullptr" << endl;
+            }
+            else
+            {
+                cout << *item << endl;
+            }
+        }
     }
 
     friend class Tester;
--- Assignments/Projects/starter files/PROJECT1-LINEAR-STRUCTURES/Tester.cpp
+++ Assignments/Projects/starter files/PROJECT1-LINEAR-STRUCTURES/Tester.cpp
@@ -1,5 +1,17 @@
 #include "Tester.hpp"
 
+/*
+This is the testers we are working on for project 1,
+but there have been modifications for the following:
+
+* Functions that used to return true on success and false on fail
+    have been updated to use exceptions, so they have no return type.
+    Therefore, these tests aren't needed and were removed.
+
+* With a linked list there is no "max value" so tests for when the
+    list was full was removed.
+*/
+
 void Tester::RunTests()
 {
     Test_IsEmpty();
@@ -113,6 +125,48 @@ void Tester::Test_IsEmpty()
     cout << "TEST: Test_IsEmpty" << endl;
 
     // Put tests here
+    {
+        // Test 1
+        cout << endl << "Test 1" << endl;
+        List<int> listA;
+        bool expectedValue = true;
+        bool actualValue = listA.IsEmpty();
+
+        cout << "Created list, didn't add anything, should be empty..." << endl;
+        cout << "Expected value: " << expectedValue << endl;
+        cout << "Actual value:   " << actualValue << endl;
+
+        if ( expectedValue == actualValue )
+        {
+            cout << "Test passed" << endl;
+        }
+        else
+        {
+            cout << "Test failed" << endl;
+        }
+    }
+
+    {
+        // Test 2
+        cout << endl << "Test 1" << endl;
+        List<int> listA;
+        listA.PushBack( 5 );
+        bool expectedValue = false;
+        bool actualValue = listA.IsEmpty();
+
+        cout << "Created list, added one thing, shouldn't be empty..." << endl;
+        cout << "Expected value: " << expectedValue << endl;
+        cout << "Actual value:   " << actualValue << endl;
+
+        if ( expectedValue == actualValue )
+        {
+            cout << "Test passed" << endl;
+        }
+        else
+        {
+            cout << "Test failed" << endl;
+        }
+    }
 }
 
 void Tester::Test_IsFull()
@@ -135,8 +189,126 @@ void Tester::Test_PushBack()
 {
     DrawLine();
     cout << "TEST: Test_PushBack" << endl;
+    {
+        cout << endl << "Test 1: Create a list, input 'A'." << endl;
 
-    // Put tests here
+        List<string> testlist;
+
+        // Things to test
+
+        bool expectedReturn = true;
+        bool actualReturn = testlist.PushBack( "A" );
+
+        int expectedSize = 1;
+        int actualSize = testlist.Size();
+
+        // Remember that Get functions return a pointer in this project.
+        string expectedValueAt0 = "A";
+        string* actualValueAt0 = testlist.Get(0);
+
+        if ( actualValueAt0 == nullptr )
+        {
+            cout << "nullptr - exiting test" << endl;
+            return;
+        }
+
+        // Display checks
+        cout << "Return, expected: " << expectedReturn << ", actual: " << actualReturn << endl;
+        cout << "Size, expected: " << expectedSize << ", actual: " << actualSize << endl;
+        cout << "Value, expected: " << expectedValueAt0 << ", actual: " << *actualValueAt0 << endl;
+
+        // Check each value
+        if ( expectedReturn != actualReturn )
+        {
+            cout << "FAIL - Bad return" << endl;
+        }
+        else if ( expectedSize != actualSize )
+        {
+            cout << "FAIL - Bad size" << endl;
+        }
+        else if ( expectedValueAt0 != *actualValueAt0 )
+        {
+            cout << "FAIL - Bad value" << endl;
+        }
+        else
+        {
+            cout << "PASS" << endl;
+        }
+    }
+
+    {
+        cout << endl << "Test 2: Push Back 'A', 'B', 'C', make sure 0 = A, 1 = B, 2 = C" << endl;
+
+        List<string> testlist;
+        testlist.PushBack( "A" );
+        testlist.PushBack( "B" );
+        testlist.PushBack( "C" );
+
+        string expectedValue_0 = "A";
+        string expectedValue_1 = "B";
+        string expectedValue_2 = "C";
+
+        string* actualValue_0 = testlist.Get( 0 );
+        string* actualValue_1 = testlist.Get( 1 );
+        string* actualValue_2 = testlist.Get( 2 );
+
+        if ( actualValue_0 == nullptr || actualValue_1 == nullptr || actualValue_2 == nullptr )
+        {
+            cout << "nullptrs returned; exiting to avoid segfault" << endl;
+            return;
+        }
+
+        cout << "Expected value at 0: " << expectedValue_0 << endl;
+        cout << "Expected value at 1: " << expectedValue_1 << endl;
+        cout << "Expected value at 2: " << expectedValue_2 << endl;
+
+        cout << "Actual value at 0: " << *actualValue_0 << endl;
+        cout << "Actual value at 1: " << *actualValue_1 << endl;
+        cout << "Actual value at 2: " << *actualValue_2 << endl;
+
+        if ( expectedValue_0 != *actualValue_0 ||
+             expectedValue_1 != *actualValue_1 ||
+             expectedValue_2 != *actualValue_2 )
+        {
+            cout << "FAIL - Bad value(s)" << endl;
+        }
+        else
+        {
+            cout << "PASS" << endl;
+        }
+    }
+
+    {
+        cout << endl << "Test 3: Create a list, insert 101 items, Size() should return 100" << endl;
+
+        List<string> testlist;
+        for ( int i = 0; i < 100; i++ )
+        {
+            testlist.PushBack( "Z" );
+        }
+
+        bool expectedReturn = false;
+        bool actualReturn = testlist.PushBack( "A" );
+
+        int expectedValue = 100;
+        int actualValue = testlist.Size();
+
+        cout << "Return, expected: " << expectedReturn << ", actual: " << actualReturn << endl;
+        cout << "Size, expected: " << expectedValue << ", actual: " << actualValue << endl;
+
+        if ( expectedValue != actualValue )
+        {
+            cout << "FAIL - Bad size" << endl;
+        }
+        else if ( expectedReturn != actualReturn )
+        {
+            cout << "FAIL - Bad return" << endl;
+        }
+        else
+        {
+            cout << "PASS" << endl;
+        }
+    }
 }
 
 void Tester::Test_PopFront()
@@ -145,6 +317,63 @@ void Tester::Test_PopFront()
     cout << "TEST: Test_PopFront" << endl;
 
     // Put tests here
+    {
+        cout << endl << "Test 1: Create an empty list. Pop should return false." << endl;
+
+        List<int> testlist;
+
+        bool expectedReturn = false;
+        bool actualReturn = testlist.PopFront();
+
+        cout << "Return, expected: " << expectedReturn << ", actual: " << actualReturn << endl;
+
+        if ( expectedReturn != actualReturn )
+        {
+            cout << "FAIL - Bad return" << endl;
+        }
+        else
+        {
+            cout << "PASS" << endl;
+        }
+    }
+
+    {
+        cout << endl << "Test 2: PushBack 'A', 'B', 'C', make sure PopFront removes correct item." << endl;
+
+        List<string> testlist;
+
+        testlist.PushBack( "A" );
+        testlist.PushBack( "B" );
+        testlist.PushBack( "C" );
+
+        bool expectedReturn = true;
+        bool actualReturn = testlist.PopFront();
+
+        string expected0 = "B";
+        string* actual0 = testlist.Get(0);
+
+        if ( actual0 == nullptr )
+        {
+            cout << "nullptr" << endl;
+            return;
+        }
+
+        cout << "Return, expected: " << expectedReturn << ", actual: " << actualReturn << endl;
+        cout << "Item at 0, expected: " << expected0 << ", actual: " << *actual0 << endl;
+
+        if ( expectedReturn != actualReturn )
+        {
+            cout << "FAIL - Bad return" << endl;
+        }
+        else if ( expected0 != *actual0 )
+        {
+            cout << "FAIL - Bad value" << endl;
+        }
+        else
+        {
+            cout << "PASS" << endl;
+        }
+    }
 }
 
 void Tester::Test_PopBack()
@@ -177,6 +406,54 @@ void Tester::Test_GetFront()
     cout << "TEST: Test_GetFront" << endl;
 
     // Put tests here
+    {
+        cout << endl << "Test 1: GetFront() from empty list should return nullptr." << endl;
+
+        List<string> testlist;
+
+        string* expectedAddress = nullptr;
+        string* actualAddress = testlist.GetFront();
+
+        cout << "Address, expected: " << expectedAddress << ", actual: " << actualAddress << endl;
+
+        if ( expectedAddress != actualAddress )
+        {
+            cout << "FAIL - Bad address" << endl;
+        }
+        else
+        {
+            cout << "PASS" << endl;
+        }
+    }
+
+    {
+        cout << endl << "Test 2: Push Back 'A', 'B', 'C', GetFront() should return A" << endl;
+
+        List<string> testlist;
+        testlist.PushBack( "A" );
+        testlist.PushBack( "B" );
+        testlist.PushBack( "C" );
+
+        string expectedValue = "A";
+        string* actualValue = testlist.GetFront();
+
+        if ( actualValue == nullptr )
+        {
+            cout << "Nullptr" << endl;
+            return;
+        }
+
+        cout << "Value, expected: " << expectedValue << ", actual: " << *actualValue << endl;
+
+        if ( *actualValue != expectedValue )
+        {
+            cout << "FAIL - Bad value" << endl;
+        }
+        else
+        {
+            cout << "PASS" << endl;
+        }
+    }
 }
 
 void Tester::Test_GetBack()
